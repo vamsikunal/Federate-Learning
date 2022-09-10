cc36539a4ae9133937717f012b074db1e3f59523
Kunal Mahahajan <kmahajan883@gmail.com>
2022-09-10 14:37:57 +0530

    Created Class DatasetLoad

--- dataset.py
+++ dataset.py
@@ -1,15 +1,33 @@
 from torchvision import datasets, transforms
 
+class DatasetLoad():
+    def __init__(self):
+        self.tranform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, ), (0.5, )),])
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, ), (0.5, )),
-])
+    def load_mnist(self):
+        train_set = datasets.MNIST(
+            "~/.pytorch/MNIST_data/", train=True, download=True, transform=self.transform)
+        test_set = datasets.MNIST(
+            "~/.pytorch/MNIST_data/", train=False, download=True, transform=self.transform)
+        return (train_set, test_set);
 
-def load_mnist():
-    train_set = datasets.MNIST(
-        "~/.pytorch/MNIST_data/", train=True, download=True, transform=transform)
-    test_set = datasets.MNIST(
-        "~/.pytorch/MNIST_data/", train=False, download=True, transform=transform)
-    return (train_set, test_set);
+    def load_cifar10(self):
+        train_set = datasets.CIFAR10(
+            "~/.pytorch/CIFAR10_data/", train=True, download=True, transform=self.transform)
+        test_set = datasets.CIFAR10(
+            "~/.pytorch/CIFAR10_data/", train=False, download=True, transform=self.transform)
+        return (train_set, test_set);
+    
+    def load_fashion_mnist(self):
+        train_set = datasets.FashionMNIST(
+            "~/.pytorch/FashionMNIST_data/", train=True, download=True, transform=self.transform)
+        test_set = datasets.FashionMNIST(
+            "~/.pytorch/FashionMNIST_data/", train=False, download=True, transform=self.transform)
+        return (train_set, test_set);
+
+
+
+
 
--- pysyft.py
+++ pysyft.py
@@ -1,6 +1,8 @@
-from dataset import *
+from dataset import DatasetLoad
 import torch
 import syft as sy  # import the syft library
+
+df = DatasetLoad()
 
 hook = sy.TorchHook(torch)  # attach the pytorch hook
 kunal = sy.VirtualWorker(hook, id="kunal")  #  remote worker 
@@ -9,11 +11,11 @@ saatvik = sy.VirtualWorker(hook, id="saatvik")
 
 def locked_data():
     federated_train_loader = sy.FederatedDataLoader(
-        load_mnist[0].federate((kunal, aayush, saatvik)), batch_size=128, shuffle=True) # the federate() method splits the data within the workers
+        df.load_mnist[0].federate((kunal, aayush, saatvik)), batch_size=128, shuffle=True) # the federate() method splits the data within the workers
 
     test_loader = torch.utils.data.DataLoader(
-        load_mnist[1], batch_size=128, shuffle=True)
+        df.load_mnist[1], batch_size=128, shuffle=True)
 
     train_loader = torch.utils.data.DataLoader(
-        load_mnist[0], batch_size=128, shuffle=True)
+        df.load_mnist[0], batch_size=128, shuffle=True)
     return (federated_train_loader, test_loader)
