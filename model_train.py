a8a8e575d8695a5c6818da4cb91bf7abf16f557c
Kunal Mahahajan <kmahajan883@gmail.com>
2022-09-12 23:31:10 +0530

    Crated model train class

--- model_train.py
+++ model_train.py
@@ -5,10 +5,10 @@ import torch.nn.functional as F
 import progressbar
 
 
-model = MyModel()
-optimizer = optim.SGD(model.parameters(), lr=0.05)
-federated_train_loader, test_loader = locked_data()
-n_epoch = 3
+# model = MyModel()
+# optimizer = optim.SGD(model.parameters(), lr=0.05)
+# federated_train_loader, test_loader = locked_data()
+# n_epoch = 3
 
 class TrainMyModel:
 
@@ -17,21 +17,19 @@ class TrainMyModel:
         self.federated_train_loader = federated_train_loader
         self.optimizer = optimizer
         self.n_epoch = n_epoch
-
+        
     def train(self):
-        bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
         loss_list = []
-        for epoch in range(n_epoch):
-            model.train()
-            for batch_idx, (data, target) in enumerate(federated_train_loader):
-                model.send(data.location) # send the model to the client device where the data is present
-                optimizer.zero_grad()         # training the model
-                output = model(data)
+        for self.epoch in range(self.n_epoch):
+            self.model.train()
+            for batch_idx, (data, target) in enumerate(self.federated_train_loader):
+                self.model.send(data.location) # send the model to the client device where the data is present
+                self.optimizer.zero_grad()         # training the model
+                output = self.model(data)
                 loss = F.cross_entropy(output, target)
                 loss.backward()
-                optimizer.step()
-                model.get() # get back the improved model
-                bar.update(batch_idx)
+                self.optimizer.step()
+                self.model.get() # get back the improved model
             loss_list += [loss.item()]
         return loss_list
 
