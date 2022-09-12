fc36dd4c51ab9fdcda50eee4dab64d91c55bb709
Kunal Mahahajan <kmahajan883@gmail.com>
2022-09-12 23:30:04 +0530

    Crated model train class

--- model_train.py
+++ model_train.py
@@ -2,24 +2,37 @@ from pysyft import *
 from model import MyModel
 import torch.optim as optim
 import torch.nn.functional as F
+import progressbar
+
 
 model = MyModel()
 optimizer = optim.SGD(model.parameters(), lr=0.05)
 federated_train_loader, test_loader = locked_data()
 n_epoch = 3
-for epoch in range(n_epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(federated_train_loader):
-        model.send(data.location) # send the model to the client device where the data is present
-        optimizer.zero_grad()         # training the model
-        output = model(data)
-        loss = F.cross_entropy(output, target)
-        loss.backward()
-        optimizer.step()
-        model.get() # get back the improved model
-        if batch_idx % 100 == 0: 
-            loss = loss.get() # get back the loss
-            print('Training Epoch: {:2d} [{:5d}/{:5d} ({:3.0f}%)]\tLoss: {:.6f}'.format(
-                epoch+1, batch_idx * 128,
-                len(federated_train_loader) * 128,
-                100. * batch_idx / len(federated_train_loader), loss.item()))
+
+class TrainMyModel:
+
+    def __init__(self, model, federated_train_loader, optimizer, n_epoch):
+        self.model = model
+        self.federated_train_loader = federated_train_loader
+        self.optimizer = optimizer
+        self.n_epoch = n_epoch
+
+    def train(self):
+        bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
+        loss_list = []
+        for epoch in range(n_epoch):
+            model.train()
+            for batch_idx, (data, target) in enumerate(federated_train_loader):
+                model.send(data.location) # send the model to the client device where the data is present
+                optimizer.zero_grad()         # training the model
+                output = model(data)
+                loss = F.cross_entropy(output, target)
+                loss.backward()
+                optimizer.step()
+                model.get() # get back the improved model
+                bar.update(batch_idx)
+            loss_list += [loss.item()]
+        return loss_list
+
+
