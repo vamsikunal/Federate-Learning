45bd4dbea6f04a7f5ba6ed65d4e28df829091e08
Kunal Mahahajan <kmahajan883@gmail.com>
2022-09-24 14:19:15 +0530

    made test_loss as class variable

--- model_eval.py
+++ model_eval.py
@@ -8,18 +8,18 @@ class ModelEvaluation:
         self.model = model
         self.test_loader = test_loader
         self.correct = 0
+        self.test_loss = 0
 
     def evluation(self):
         self.model.eval()
-        test_loss = 0
         with torch.no_grad():
             for data, target in self.test_loader:
                 output = self.model(data)
-                test_loss += F.cross_entropy(output, target, reduction='sum').item()
+                self.test_loss += F.cross_entropy(output, target, reduction='sum').item()
                 pred = output.argmax(1, keepdim=True) # get the index of the max log-probability
                 self.correct += pred.eq(target.view_as(pred)).sum().item()
     
     def accuracy(self):
-        test_loss /= len(self.test_loader.dataset)
+        self.test_loss /= len(self.test_loader.dataset)
         accuracy = 100. * self.correct / len(self.test_loader.dataset)
         return accuracy
