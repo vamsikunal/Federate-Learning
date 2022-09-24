b8ce07c6b2f83be6e7bad6a02048562aafa4083e
Kunal Mahahajan <kmahajan883@gmail.com>
2022-09-24 14:18:40 +0530

    changed optimizer attribute to class

--- model_train.py
+++ model_train.py
@@ -5,17 +5,12 @@ import torch.nn.functional as F
 import progressbar
 
 
-# model = MyModel()
-# optimizer = optim.SGD(model.parameters(), lr=0.05)
-# federated_train_loader, test_loader = locked_data()
-# n_epoch = 3
-
 class TrainMyModel:
 
     def __init__(self, model, federated_train_loader, n_epoch):
         self.model = model
         self.federated_train_loader = federated_train_loader
-        self.optimizer = model.optimizer
+        self.optimizer = model.optimizer()
         self.n_epoch = n_epoch
         self.loss_list = []
         
@@ -23,7 +18,7 @@ class TrainMyModel:
         for self.epoch in range(self.n_epoch):
             print("Epoch: ", self.epoch, "\n")
             self.model.train()
-            with progressbar.ProgressBar(max_value=batch_idx) as bar:
+            with progressbar.ProgressBar(max_value=progressbar.UnknownLength) as bar:
                 for batch_idx, (data, target) in enumerate(self.federated_train_loader):
                     self.model.send(data.location) # send the model to the client device where the data is present
                     self.optimizer.zero_grad()         # training the model
